873c903c620dfe811d0d64ce88633ef6c7862e4e
yunxilyf <2574379425@qq.com>
2024-12-08 10:39:19 +0800

    更新

--- flutter/lib/desktop/pages/connection_page.dart
+++ flutter/lib/desktop/pages/connection_page.dart
@@ -83,26 +83,26 @@ class _OnlineStatusWidgetState extends State<OnlineStatusWidget> {
                 _svcIsUsingPublicServer.value),
             child: Row(
               crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(', ', style: TextStyle(fontSize: em)),
-                Flexible(
-                  child: InkWell(
-                    onTap: onUsePublicServerGuide,
-                    child: Row(
-                      children: [
-                        Flexible(
-                          child: Text(
-                            translate('setup_server_tip'),
-                            style: TextStyle(
-                                decoration: TextDecoration.underline,
-                                fontSize: em),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                )
-              ],
+              // children: [
+              //   Text(', ', style: TextStyle(fontSize: em)),
+              //   Flexible(
+              //     child: InkWell(
+              //       onTap: onUsePublicServerGuide,
+              //       child: Row(
+              //         children: [
+              //           Flexible(
+              //             child: Text(
+              //               translate('setup_server_tip'),
+              //               style: TextStyle(
+              //                   decoration: TextDecoration.underline,
+              //                   fontSize: em),
+              //             ),
+              //           ),
+              //         ],
+              //       ),
+              //     ),
+              //   )
+              // ],
             ),
           ),
         );
