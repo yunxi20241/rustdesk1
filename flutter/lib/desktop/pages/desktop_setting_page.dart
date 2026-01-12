1979f055b84e0481305377c6d18e730b1b03abed
yunxilyf <2574379425@qq.com>
2026-01-12 18:22:01 +0800

    add

--- flutter/lib/desktop/pages/desktop_setting_page.dart
+++ flutter/lib/desktop/pages/desktop_setting_page.dart
@@ -2347,7 +2347,7 @@ class _AboutState extends State<_About> {
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           Text(
-                            'Copyright © ${DateTime.now().toString().substring(0, 4)} Purslane Ltd.\n$license',
+                            'Copyright © ${DateTime.now().toString().substring(0, 4)} YUNXI \n$license',
                             style: const TextStyle(color: Colors.white),
                           ),
                           Text(
